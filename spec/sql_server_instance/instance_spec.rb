05e88679f15058e896dff00fddcc73faf38ab1a5
Steven Murawski <steven.murawski@gmail.com>
2015-06-30 17:18:38 -0500

    fix test

--- spec/sql_server_instance/instance_spec.rb
+++ spec/sql_server_instance/instance_spec.rb
@@ -11,22 +11,9 @@ describe 'minimal::instance' do
     it 'creates the "new" sql server instance' do
       expect(chef_run).to create_sql_server_instance('new')
     end
-  end
-end
-
-describe 'sql_server::default' do 
-  
-  let(:chef_run) do 
-    runner = ChefSpec::SoloRunner.new(step_into: ['sql_server_instance'])
-    runner.converge(described_recipe)
-  end
-  
-  context 'when compiling the test recipe' do
-    it 'creates the "new" sql server instance' do
-      expect(chef_run).to create_sql_server_instance('new')
-    end
     it 'creates a directory for the ConfigurationFile.ini' do
       expect(chef_run).to create_directory('c:/sql').with(path: 'c:/sql')
     end
   end
 end
+
